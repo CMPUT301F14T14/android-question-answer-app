1e49a27930cedde84293014e65ddb7363f1d62aa
Kopo101 <cbotto@ualberta.ca>
2014-10-16 13:30:19 -0600

    added Comment class

---
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/model/Comment.java
@@ -0,0 +1,12 @@
+package ca.ualberta.cs.cmput301f14t14.questionapp.model;
+
+public class Comment {
+	
+	private String body;
+	private String userName;
+	
+	public Comment(String text, String createdBy) {
+		body = text;
+		createdBy = userName;
+	}
+}
