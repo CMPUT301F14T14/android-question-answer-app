2eb66ccb20894b3914a1526e9c696fe22a26312c
Kopo101 <cbotto@ualberta.ca>
2014-10-16 18:43:24 -0600

    added remote datastore tests

--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/model/RemoteDataStore.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/model/RemoteDataStore.java
@@ -2,4 +2,14 @@ package ca.ualberta.cs.cmput301f14t14.questionapp.model;
 
 public class RemoteDataStore {
 
+	public boolean hasAccess() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void pushQuestion(Question mQuestion) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- QuestionAppTests/src/ca/ualberta/cs/cmput301f14t14/questionapp/test/RemoteDataStoreTest.java
+++ QuestionAppTests/src/ca/ualberta/cs/cmput301f14t14/questionapp/test/RemoteDataStoreTest.java
@@ -4,17 +4,31 @@ import ca.ualberta.cs.cmput301f14t14.questionapp.model.Answer;
 import ca.ualberta.cs.cmput301f14t14.questionapp.model.DataManager;
 import ca.ualberta.cs.cmput301f14t14.questionapp.model.Image;
 import ca.ualberta.cs.cmput301f14t14.questionapp.model.Question;
+import ca.ualberta.cs.cmput301f14t14.questionapp.model.RemoteDataStore;
 import junit.framework.TestCase;
 
 public class RemoteDataStoreTest extends TestCase{
 
+	private RemoteDataStore mRemoteStore;
+	private Question mQuestion;
+	
 	protected void setUp() throws Exception {
 		super.setUp();
+		mRemoteStore = new RemoteDataStore();
+		mQuestion = new Question("TITLE", "BODY", null);
+		
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
 	
+	public void testRemoteAccess() {
+		assertTrue(mRemoteStore.hasAccess());
+	}
+	
+	public void testPushQuestion() {
+		mRemoteStore.pushQuestion(mQuestion);
+	}
 	
 }
