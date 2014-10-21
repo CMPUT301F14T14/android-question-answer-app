f22106561d8471bfcd47b9a22c23620f0321e53f
sdwowk <sdwowk@ualberta.ca>
2014-10-21 15:36:06 -0700

    Create fragment class for username

---
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/view/UsernameFragment.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cs.cmput301f14t14.questionapp.view;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UsernameFragment extends Fragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
+		return null;
+	}
+
+}
