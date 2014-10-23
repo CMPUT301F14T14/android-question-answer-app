30830b665cbc5554116c823683dbe8606fe9052e
sdwowk <sdwowk@ualberta.ca>
2014-10-22 23:39:46 -0700

    Added functionality (onclick listeners) to files

--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/WelcomesScreenActivity.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/WelcomesScreenActivity.java
@@ -2,8 +2,8 @@ package ca.ualberta.cs.cmput301f14t14.questionapp;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import ca.ualberta.cs.cmput301f14t14.questionapp.view.UsernameFragment;
 
 public class WelcomesScreenActivity extends FragmentActivity {
 
@@ -13,22 +13,9 @@ public class WelcomesScreenActivity extends FragmentActivity {
 		setContentView(R.layout.welcome_screen);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.welcomes_screen, menu);
-		return true;
+	public void onCreateUsername(View view){
+		UsernameFragment userFragment = new UsernameFragment();
+		
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	
 }
--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/view/UsernameFragment.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/view/UsernameFragment.java
@@ -1,16 +1,41 @@
 package ca.ualberta.cs.cmput301f14t14.questionapp.view;
 
+import ca.ualberta.cs.cmput301f14t14.questionapp.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UsernameFragment extends Fragment {
 	
+	public UsernameFragment(){
+		
+	}
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
-		return null;
+		View newView = inflater.inflate(R.layout.welcome_screen, container, false);
+		
+		Button submitUsernameButton = (Button) newView.findViewById(R.id.enterUsernameButton);
+		
+		submitUsernameButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				EditText userText = (EditText) arg0.findViewById(R.id.usernameText);
+				String username = userText.getText().toString();
+				
+				Toast.makeText(getActivity(), "Welcome " + username + " to Qasper", Toast.LENGTH_SHORT).show();
+				
+			}
+			
+		});
+		return newView;
 	}
 
 }
