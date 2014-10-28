72e2aec43be9f9c4ce3127d073bc14b32b4cc9de
Ryan Kusters <kusters@ualberta.ca>
2014-10-28 10:34:23 -0600

    filled out more of the Datamanager methods, added two methods for basic Local store

--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/DataManager.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/DataManager.java
@@ -1,5 +1,6 @@
 package ca.ualberta.cs.cmput301f14t14.questionapp;
 
+import java.util.Iterator;
 import java.util.List;
 
 import ca.ualberta.cs.cmput301f14t14.questionapp.model.Answer;
@@ -22,59 +23,78 @@ public class DataManager {
 	
 	//View Interface Begins
 	public void addQuestion(Question validQ) {
-		//questionList = localDataStore.getQuestionList();
-		/*Implement Method*/
-		//localDataStore.save(questionList);
+		questionList = localDataStore.getQuestionList();
+		questionList.add(validQ);
+		localDataStore.save(questionList);
 		
 	}
 
 	public Question getQuestion(Integer id) {
-		//questionList = localDataStore.getQuestionList();
-		/*Implement Method*/
+		questionList = localDataStore.getQuestionList();
+		Iterator<Question> list = questionList.iterator();
+		while(list.hasNext()){
+			Question question = list.next();
+			Integer qid = question.getId();
+			if(id.equals(qid)){
+				return question;
+			}
+		}
 		return null;
 	}
 
 	public void addAnswer(Integer Qid, Answer A){
-		//questionList = localDataStore.getQuestionList();
-		/*Implement Method*/
-		//localDataStore.save(questionList);
+		questionList = localDataStore.getQuestionList();
+		Question question = getQuestion(Qid);
+		Integer position = questionList.indexOf(question);
+		question.addAnswer(A);
+		questionList.set(position, question);
+		localDataStore.save(questionList);
 	}
 	
 	public Answer getAnswer(Integer Qid, Integer Aid) {
-		//questionList = localDataStore.getQuestionList();
-				/*Implement Method*/
-		return null;
+		questionList = localDataStore.getQuestionList();
+		Question question = getQuestion(Qid);
+		Answer answer = question.getAnswer(Aid);
+		return answer;
 	}
 	
 	public void addQuestionComment(Integer Qid, Comment C){
-		//questionList = localDataStore.getQuestionList();
-		/*Implement Method*/
-		//localDataStore.save(questionList);
+		questionList = localDataStore.getQuestionList();
+		Question question = getQuestion(Qid);
+		Integer position = questionList.indexOf(question);
+		question.addComment(C);
+		questionList.set(position, question);
+		localDataStore.save(questionList);
 	}
 
 	public Comment getQuestionComment(Integer Qid, Integer cid) {
-		//questionList = localDataStore.getQuestionList();
-				/*Implement Method*/
-		return null;
+		questionList = localDataStore.getQuestionList();
+		Question question = getQuestion(Qid);
+		Comment comment = question.getComment(cid);
+		return comment;
 	}
 	
 	public void addAnswerComment(Integer Qid, Integer Aid, Comment C){
-		//questionList = localDataStore.getQuestionList();
-		/*Implement Method*/
-		//localDataStore.save(questionList);
+		questionList = localDataStore.getQuestionList();
+		Question question = getQuestion(Qid);
+		Integer position = questionList.indexOf(question);
+		Answer answer = question.getAnswer(Aid);
+		answer.addComment(C);
+		question.setAnswer(Aid,A);
+		questionList.set(position, question);
+		localDataStore.save(questionList);
 	}
 	
 	public Comment getAnswerComment(Integer Qid, Integer Aid, Integer Cid){
-		//questionList = localDataStore.getQuestionList();
+		questionList = localDataStore.getQuestionList();
 				/*Implement Method*/
 		return null;
 	}
 	
 	public List<Question> load(){
-		//questionList = localDataStore.getQuestionList();
+		questionList = localDataStore.getQuestionList();
 		return questionList;
 	}
-	
 
 	public void favoriteQuestion(Integer Qid) {
 		// TODO Auto-generated method stub
