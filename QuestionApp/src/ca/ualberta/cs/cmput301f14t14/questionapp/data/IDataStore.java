af8c3d4ee559e41f3d5dda07485f710b50d2a45b
Stephen Just <stephenjust@gmail.com>
2014-11-03 14:01:21 -0700

    Implement LocalDataStore.
    Use Maps to store stuff, for fast lookup.

--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/data/IDataStore.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/data/IDataStore.java
@@ -5,19 +5,21 @@ import java.util.UUID;
 
 import ca.ualberta.cs.cmput301f14t14.questionapp.model.Answer;
 import ca.ualberta.cs.cmput301f14t14.questionapp.model.Comment;
+import ca.ualberta.cs.cmput301f14t14.questionapp.model.Model;
 import ca.ualberta.cs.cmput301f14t14.questionapp.model.Question;
 
 public interface IDataStore {
 
 	public void putQuestion(Question question);
+	public Question getQuestion(UUID id);
 
 	public void putAnswer(Answer answer);
+	public Answer getAnswer(UUID id);
 
-	public void putComment(Comment comment);
-
-	public boolean isQuestion(UUID id);
-
-	public boolean isAnswer(UUID id);
+	public void putQComment(Comment<Question> comment);
+	public void putAComment(Comment<Answer> comment);
+	public Comment<Question> getQComment(UUID id);
+	public Comment<Answer> getAComment(UUID id);
 
 	public List<Question> getQuestionList();
 
--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/data/LocalDataStore.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/data/LocalDataStore.java
@@ -1,6 +1,9 @@
 package ca.ualberta.cs.cmput301f14t14.questionapp.data;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import ca.ualberta.cs.cmput301f14t14.questionapp.model.Answer;
@@ -8,49 +11,64 @@ import ca.ualberta.cs.cmput301f14t14.questionapp.model.Comment;
 import ca.ualberta.cs.cmput301f14t14.questionapp.model.Question;
 
 public class LocalDataStore implements IDataStore {
-	private String textSave = "localSave";
-	private List<Question> questionList;
-	
+
+	private Map<UUID, Question> questions;
+	private Map<UUID, Answer> answers;
+	private Map<UUID, Comment<Question>> qcomments;
+	private Map<UUID, Comment<Answer>> acomments;
+
+	public LocalDataStore() {
+		questions = new HashMap<UUID, Question>();
+		answers = new HashMap<UUID, Answer>();
+		qcomments = new HashMap<UUID, Comment<Question>>();
+		acomments = new HashMap<UUID, Comment<Answer>>();
+	}
+
 	public List<Question> getQuestionList(){
-		return questionList;
+		return new ArrayList<Question>(questions.values());
 	}
 	
 	public void save(List<Question> questionList){
-		this.questionList = questionList;
+		// TODO Auto-generated method stub
 	}
 
-	public void putQuestion(Question mQuestion) {
-		// TODO Auto-generated method stub
-		
+	public void putQuestion(Question question) {
+		questions.put(question.getId(), question);
 	}
 
-	public void putAnswer(Answer mAnswer) {
-		// TODO Auto-generated method stub
-		
+	public Question getQuestion(UUID id) {
+		return questions.get(id);
 	}
 
-	public void putComment(Comment mComment) {
-		// TODO Auto-generated method stub
-		
+	public void putAnswer(Answer answer) {
+		answers.put(answer.getId(), answer);
+	}
+	
+	public Answer getAnswer(UUID id) {
+		return answers.get(id);
 	}
 
-	public boolean isFavorite(UUID uuid) {
-		// TODO Auto-generated method stub
-		return false;
+	public void putQComment(Comment<Question> comment) {
+		qcomments.put(comment.getId(), comment);
+	}
+	
+	public Comment<Question> getQComment(UUID id) {
+		return qcomments.get(id);
 	}
 
-	public boolean isQuestion(UUID id) {
-		// TODO Auto-generated method stub
-		return false;
+	public void putAComment(Comment<Answer> comment) {
+		acomments.put(comment.getId(), comment);
 	}
-
-	public boolean isAnswer(UUID id) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public Comment<Answer> getAComment(UUID id) {
+		return acomments.get(id);
 	}
 
 	public void clear() {
-		// Empty the data store
+		questions.clear();
+		answers.clear();
+		qcomments.clear();
+		acomments.clear();
 	}
 	
 }
