881ad0c6593ddaf3642561d89a83d9e7abcb6de2
sdwowk <sdwowk@ualberta.ca>
2014-11-17 17:56:43 -0700

    Get rid of settings bar in search activity

--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/MainActivity.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/MainActivity.java
@@ -172,6 +172,7 @@ public class MainActivity extends Activity {
 		}
 		return qlist;
     }
+    
 	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
