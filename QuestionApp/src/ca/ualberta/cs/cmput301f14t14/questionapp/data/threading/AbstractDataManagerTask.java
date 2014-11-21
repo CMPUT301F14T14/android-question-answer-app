4014a757c19b71e700ad4f5ab0de14a81cacbafe
Stephen Just <stephenjust@gmail.com>
2014-11-20 22:47:52 -0700

    Refactor AbstractDataManagerTask

--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/data/threading/AbstractDataManagerTask.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/data/threading/AbstractDataManagerTask.java
@@ -1,29 +1,39 @@
 package ca.ualberta.cs.cmput301f14t14.questionapp.data.threading;
 
 import ca.ualberta.cs.cmput301f14t14.questionapp.data.Callback;
-import ca.ualberta.cs.cmput301f14t14.questionapp.model.Question;
 import android.content.Context;
 import android.os.AsyncTask;
 
+/**
+ * An AsyncTask that will run the provided callback in its onPostExecute section
+ * @param <S> Task parameter type
+ * @param <T> Task progress response type
+ * @param <V> Task return value type
+ */
 public abstract class AbstractDataManagerTask<S,T,V> extends AsyncTask<S,T,V> {
 	
 	protected Context context;
+	protected Callback<V> callback = null;
 	
 	public AbstractDataManagerTask(Context c) {
-		context = c;
+		this(c, null);
+	}
+
+	public AbstractDataManagerTask(Context context, Callback<V> callback) {
+		this.context = context;
+		this.callback = callback;
 	}
 	
 	protected Context getContext() {
 		return context;
 	}
 	
-	Callback<V> callback = null;
-	
 	public void setCallBack(Callback<V> c) {
 		callback = c;
 	}
-	/** Runs doInBackground in the current thread.
-	 * 
+
+	/**
+	 * Runs doInBackground in the current thread.
 	 * @param param
 	 * @return
 	 */
