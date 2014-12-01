16692e795864825cfe366da6f696808d7b94f10e
Stephen Just <stephenjust@gmail.com>
2014-12-01 01:16:13 -0700

    First batch of refactoring and commenting for ClientData class.

--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/data/ClientData.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/data/ClientData.java
@@ -14,24 +14,35 @@ import android.util.Log;
 /**
  * This class handles data that only exists on the client, and is
  * never transferred to the web service.
- * @author Stephen Just
+ * 
+ * The back-end for this data store is Android SharedPreferences.
  */
 public class ClientData {
+
 	private static final String PREF_SET = "cs.ualberta.cs.cmput301f14t14.questionapp.prefs";
 	private static final String VAL_USERNAME = "username";
+	private static final String VAL_FAVORITES_LIST = "favqlist";
+	private static final String VAL_READ_LATER_LIST = "readlaterlist";
+	private static final String VAL_UPVOTED_LIST = "upvotelist";
 	
 	private SharedPreferences prefs;
-	
+
 	public ClientData(Context context) {
 		this.prefs = context.getSharedPreferences(PREF_SET, Context.MODE_PRIVATE);
 	}
-	
 
-
+	/**
+	 * Get username record
+	 * @return
+	 */
 	public String getUsername() {
 		return this.prefs.getString(VAL_USERNAME, null);
 	}
-	
+
+	/**
+	 * Set username record
+	 * @param username
+	 */
 	public void setUsername(String username) {
 		username = username.trim();
 		if (username.length() == 0) {
@@ -41,48 +52,36 @@ public class ClientData {
 		e.putString(VAL_USERNAME, username);
 		e.apply();
 	}
-	
-	public void clear() {
-		Editor e = prefs.edit();
-		e.clear();
-		e.apply();
-	}
-
-	
 
 	public void saveFavoriteQuestions(List<UUID> list){
-		saveItems(list, "favqlist");
+		saveItems(list, VAL_FAVORITES_LIST);
 	}
 	
-	public ArrayList<UUID> getFavoriteQuestions() {
-		return getItems("favqlist");
+	public List<UUID> getFavoriteQuestions() {
+		return getItems(VAL_FAVORITES_LIST);
 	}
 	
-	public ArrayList<UUID> getReadLaterQuestions() {
-		return getItems(readlaterfilename);
+	public List<UUID> getReadLaterQuestions() {
+		return getItems(VAL_READ_LATER_LIST);
 	}
-	
-	
-	String readlaterfilename = "readlaterlist";
-	String upvoteditems = "upvotelist";
 	
 	public void markQuestionReadLater(UUID u) {
-		List<UUID> appendlist = getItems(readlaterfilename);
+		List<UUID> appendlist = getItems(VAL_READ_LATER_LIST);
 		appendlist.add(u);
-		saveItems(appendlist, readlaterfilename);
+		saveItems(appendlist, VAL_READ_LATER_LIST);
 	}
 	
 	public void unmarkQuestionReadLater(UUID u){
-		List<UUID> list = getItems(readlaterfilename);
+		List<UUID> list = getItems(VAL_READ_LATER_LIST);
 		list.remove(u);
-		saveItems(list,readlaterfilename);
+		saveItems(list,VAL_READ_LATER_LIST);
 	}
 	public boolean isQuestionReadLater(UUID id) {
 		/** Call this to see if your desired question is
 		 *  marked as read later. After that, the user will
 		 *  call the datamanager to get the question itself
 		 */  
-		List<UUID> rllist = getItems(readlaterfilename);
+		List<UUID> rllist = getItems(VAL_READ_LATER_LIST);
 		if (rllist.contains(id)){
 			return true; 
 		} else {
@@ -93,52 +92,66 @@ public class ClientData {
 	}
 	
 	public void markItemUpvoted(UUID id) {
-		List<UUID> upvoteList = getItems(upvoteditems);
+		List<UUID> upvoteList = getItems(VAL_UPVOTED_LIST);
 		upvoteList.add(id);
-		saveItems(upvoteList, upvoteditems);
+		saveItems(upvoteList, VAL_UPVOTED_LIST);
 	}
 	
 	public boolean isItemUpvoted(UUID id) {
-		List<UUID> list = getItems(upvoteditems);
+		List<UUID> list = getItems(VAL_UPVOTED_LIST);
 		return (list.contains(id)) ? true : false;
 	}
-	public ArrayList<UUID> getItems(String filename) {
-		//Supposed to return a list of favorite questions
-		//We can then add to this list
-		//Pull a list of favorited questions from SharedPrefs		
-		Set<String> favset = prefs.getStringSet(filename, null);
+	
+	/**
+	 * Get a list of UUIDs from a named list
+	 * @param name Name of list
+	 * @return List of UUIDs
+	 */
+	public List<UUID> getItems(String name) {
+		// Get set of data from SharedPreferences
+		Set<String> set = prefs.getStringSet(name, null);
 
-
-		if (favset == null ){ //Only on first run of the app this will be null
+		// Set may be null on first run of app
+		if (set == null) {
 			return new ArrayList<UUID>();
 		}
-		//Need to build up a new set of UUIDs before converting to List
-		Set<UUID> intset = new HashSet<UUID>();
-		for (String s : favset) {
-			try {
-				intset.add(UUID.fromString(s));
-			} catch (NullPointerException e){
-				Log.e("ClientData", "Error converting UUID from String to UUID");
-			}
+
+		// Need to build up a new set of UUIDs before converting to List
+		Set<UUID> intermediateSet = new HashSet<UUID>();
+		for (String s: set) {
+			if (s == null) continue;
+			intermediateSet.add(UUID.fromString(s));
 		}
+
+		// Construct the final list
 		ArrayList<UUID> returnlist = new ArrayList<UUID>();
-		returnlist.addAll(intset);
+		returnlist.addAll(intermediateSet);
 		return returnlist;
 	}
 
-	
-	public void saveItems(List<UUID> list, String filename) {
-		//Saves the list to sharedPreferences.
-		
+	/**
+	 * Save a list of UUIDs to a named list
+	 * @param list List of UUIDs
+	 * @param name Name of list
+	 */
+	public void saveItems(List<UUID> list, String name) {
 		Editor e = prefs.edit();
-		Set<String> favset = new HashSet<String>();
-		
-		for (UUID i : list) {
-			favset.add(i.toString());
+		Set<String> set = new HashSet<String>();
+
+		for (UUID i: list) {
+			set.add(i.toString());
 		}
-		e.putStringSet(filename, favset);
+		e.putStringSet(name, set);
 		e.commit();
 	}
 
+	/**
+	 * Clear all data in this store
+	 */
+	public void clear() {
+		Editor e = prefs.edit();
+		e.clear();
+		e.apply();
+	}
 
 }
