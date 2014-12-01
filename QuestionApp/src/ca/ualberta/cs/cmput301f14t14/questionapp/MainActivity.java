75935a492d19b69e0ccde3850001fb80d237fb6f
lamorie <lamorie@ualberta.ca>
2014-11-30 23:37:57 -0700

    things dumb people do

--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/MainActivity.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/MainActivity.java
@@ -1,13 +1,11 @@
 package ca.ualberta.cs.cmput301f14t14.questionapp;
 
 import java.io.File;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
-
 import ca.ualberta.cs.cmput301f14t14.questionapp.data.Callback;
 import ca.ualberta.cs.cmput301f14t14.questionapp.data.ClientData;
 import ca.ualberta.cs.cmput301f14t14.questionapp.data.DataManager;
@@ -29,12 +27,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 
 public class MainActivity extends Activity {
 
