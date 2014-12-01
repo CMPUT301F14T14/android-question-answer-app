4440236efada79ebcf7c72bdd8d27b0f9f3539d7
Stephen Just <stephenjust@gmail.com>
2014-12-01 00:58:27 -0700

    Added docs to Callback interface

--- QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/data/Callback.java
+++ QuestionApp/src/ca/ualberta/cs/cmput301f14t14/questionapp/data/Callback.java
@@ -1,6 +1,17 @@
 package ca.ualberta.cs.cmput301f14t14.questionapp.data;
 
+/**
+ * Interface for DataManager call-backs.
+ *
+ * run() will be called by the onPostExecute method of AsyncTasks
+ * within implementations of DataManager functions.
+ *
+ * @param <T> Return type of DataManager call.
+ */
 public interface Callback<T> {
-	public void run(T o);  //Super dangerous, but the callback when created should know
-								//What to expect when casting
+	/**
+	 * Run by the onPostExecute method of a Task
+	 * @param object
+	 */
+	public void run(T object);
 }
